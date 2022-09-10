aea6c2c4e3af9b06c5e7fe63b128a237f45fde8f
vidyasager162 <98201849+vidyasager162@users.noreply.github.com>
2022-09-10 11:13:48 +0530

    Except Login form, everything works!!

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-window.onscroll = function() {scrollFunction()};
+function myFunction(){
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+}
+function openNav(){
+	document.getElementById("sidenavbar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+}
 
-function scrollFunction(){
-	if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-		document.getElementById("navbar").style.top = "0";
-	} else {
-		document.getElementById("navbar").style.top = "-70px";
-	}
+function closeNav(){
+	document.getElementById("sidenavbar").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
 }
